e951320a736a41b8ed3b39bb136c743a04a3cd27
Zdeněk Lapeš <lapes.zdenek@gmail.com>
2024-12-14 16:32:14 +0100

    .

--- main.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Call it from root directory
-
-source ./scripts/const.sh
-source ./scripts/docker.sh
-source ./scripts/utils.sh
--- make.sh
+++ make.sh
@@ -1,3 +1,32 @@
+#!/bin/bash
+
+
+RM="rm -rfd"
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+function prune_docker() {
+    # Stop and remove all containers
+    docker stop $(docker ps -aq)
+    docker system prune --all --force --volumes
+
+    # Remove all volumes: not just dangling ones
+    for i in $(docker volume ls --format json | jq -r ".Name"); do
+        docker volume rm -f ${i}
+    done
+}
+
+function docker_show_ipaddress() {
+    # Show ip address of running containers
+    for docker_container in $(docker ps -aq); do
+        CMD1="$(docker ps -a | grep "${docker_container}" | grep --invert-match "Exited\|Created" | awk '{print $2}'): "
+        if [ ${CMD1} != ": " ]; then
+            printf "${CMD1}"
+            printf "$(docker inspect ${docker_container} | grep "IPAddress" | tail -n 1)\n"
+        fi
+    done
+}
+
 function clean() {
     # Clean project
     ${RM} *.zip
